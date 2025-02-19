6d581d11d62157091c107677eb3e9ad2f6884a49
Thomas Wiesner <485781+tomw1808@users.noreply.github.com>
2025-02-19 14:05:50 +0100

    refactor: Update OpenZeppelin import paths to use local lib directory

--- contracts/src/UspdToken.sol
+++ contracts/src/UspdToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "../lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
 import "./PriceOracle.sol";
 import {UspdStabilizerToken} from "./UspdStabilizerToken.sol";
 
