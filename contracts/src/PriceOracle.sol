062da87cc88cdb6b1a73bc70523b0a42744b865a
Thomas Wiesner <485781+tomw1808@users.noreply.github.com>
2025-02-21 08:57:52 +0000

    refactor: Remove UUPS-related code from PriceOracle and PriceOracleProxy

--- contracts/src/PriceOracle.sol
+++ contracts/src/PriceOracle.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "./oracle/OracleEntrypoint.sol";
 import "./PriceOracleStorage.sol";
 
@@ -19,10 +18,8 @@ contract PriceOracle is
     Initializable, 
     ERC721Upgradeable,
     PausableUpgradeable,
-    AccessControlUpgradeable,
-    UUPSUpgradeable 
+    AccessControlUpgradeable
 {
-    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
     struct ResponseWithExpenses {
         uint value;
         uint expenses;
@@ -71,7 +68,6 @@ contract PriceOracle is
         __UUPSUpgradeable_init();
 
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(UPGRADER_ROLE, msg.sender);
 
         oracleEntrypoint = _oracleEntrypoint;
         priceProvider = _priceProvider;
@@ -79,11 +75,6 @@ contract PriceOracle is
         priceStalenessPeriod = _priceStalenessPeriod;
     }
 
-    function _authorizeUpgrade(address newImplementation)
-        internal
-        override
-        onlyRole(UPGRADER_ROLE)
-    {}
 
     function supportsInterface(bytes4 interfaceId)
         public
